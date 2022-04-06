eee370dab0ca14e7d1a0cead260a7b4a6230755f
Ethan-Hatch <101227468+Ethan-Hatch@users.noreply.github.com>
2022-04-06 16:06:51 -0600

    Add files via upload

---
+++ SeleniumTests/TestScript.java
@@ -0,0 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class TestScript {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            //System.out.println(envName + ": " + env.get(envName));
+            System.out.format("%s=%s%n", envName, env.get(envName));
+        }
+//        ChromeOptions options = new ChromeOptions();
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver(options);
+
+    }
+}
