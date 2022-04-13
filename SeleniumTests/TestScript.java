36da706ffdeb1d86d150b2785226bacebb496ab5
Ethan-Hatch <101227468+Ethan-Hatch@users.noreply.github.com>
2022-04-13 08:49:51 -0600

    Update TestScript.java

--- SeleniumTests/TestScript.java
+++ SeleniumTests/TestScript.java
@@ -1,5 +1,5 @@
 // import io.github.bonigarcia.wdm.WebDriverManager;
-package org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
 // import org.openqa.selenium.chrome.ChromeDriver;
 // import org.openqa.selenium.chrome.ChromeOptions;
 // import org.testng.annotations.Test;
